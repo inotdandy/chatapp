a9abd247adc7914257f3d77aa98f41adadacb9ea
inotdandy <dandy.inot1990@gmail.com>
2021-06-20 19:38:25 +0800

    send new message, display message by room id

--- resources/js/Pages/Chat/messageContainer.vue
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>Message Container</div>
+    <div class="h-96 w-full">
+        <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+            <message-item
+                v-for="(message, index) in messages"
+                :key="index"
+                :message="message"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
+import MessageItem from './messageItem.vue'
 export default {
-    
+    props: ['messages'],
+    components: {
+        MessageItem
+    }
 }
 </script>
